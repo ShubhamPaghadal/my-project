32f2204d113b0f1cb3addcba786e2db7d696aec0
ShubhamPaghadal <shubham10.tps@gmail.com>
2025-04-04 11:12:46 +0530

    Touch

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,105 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css'
-import Home from './Components/Home'
-import Layouts from './Components/Layouts';
-import Products from './Components/Products';
-import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home";
+import Layouts from "./Components/Layouts";
+import Products from "./Components/Products";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Toggle_item from './Components/Toggle_item'
-import Drop_down from './Components/Drop_down';
-import Wishlist from './Components/Wishlist';
-
+import Toggle_item from "./Components/Toggle_item";
+import Drop_down from "./Components/Drop_down";
+import Wishlist from "./Components/Wishlist";
+import { toast } from "react-toastify";
 
 function App() {
-  const [add_item, setadd_item] = useState([])
-  const [add_wishlist, setadd_wishlist] = useState([])
-  const [product, setproduct] = useState([])
-  const [query, setquery] = useState('')
-  const [loading, setloading] = useState(false)
-  const filter_ittems =  product.filter((item) => item.title.toLowerCase().includes( query))
-  
-    // const filter_poducts = product.filter((item) => item.title.toLowerCase().includes( "men's clothing"))
-    // console.log('filter_poducts.....',filter_poducts )
+  const [add_item, setadd_item] = useState([]);
+  const [add_wishlist, setadd_wishlist] = useState([]);
+  const [product, setproduct] = useState([]);
+  const [query, setquery] = useState("");
+  const [loading, setloading] = useState(false);
+  const filter_ittems = product.filter((item) =>
+    item.title.toLowerCase().includes(query)
+  );
 
-console.log('add_wishlist.......',add_wishlist)
-    
-    useEffect( () => {
-      setloading(true)
-      let product_details = async () => {
-        let api =  await axios("https://fakestoreapi.com/products");
-      setproduct(api.data)
-      }
-      product_details()
-     setTimeout(() => {
-      setloading(false)
-     }, 2000);
-    }, [])
-    console.log('product............',product)
-  
-  console.log(add_item)
-  console.log('....',add_item)
+  // const filter_poducts = product.filter((item) => item.title.toLowerCase().includes( "men's clothing"))
+  // console.log('filter_poducts.....',filter_poducts )
 
-  const  add_cart  = (item) => {
-          setadd_item ([...add_item,item])
-  }
+  console.log("add_wishlist.......", add_wishlist);
 
+  useEffect(() => {
+    setloading(true);
+    let product_details = async () => {
+      let api = await axios("https://fakestoreapi.com/products");
+      setproduct(api.data);
+    };
+    product_details();
+    setTimeout(() => {
+      setloading(false);
+    }, 2000);
+  }, []);
+  console.log("product............", product);
 
-  const add_wishlist_cart  = (item) => {
-         setadd_wishlist ([...add_wishlist,item])
-  }
+  console.log(add_item);
+  console.log("....", add_item);
+
+  const add_cart = (item) => {
+    setadd_item([...add_item, item]);
+    toast("add cart successfully!");
+  };
+
+  const add_wishlist_cart = (item) => {
+    setadd_wishlist([...add_wishlist, item]);
+    toast("add favorite cart successfully!");
+  };
 
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-        <Route path='/'  element={<Layouts add_wishlist={add_wishlist} add_item={add_item} setquery=
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Layouts
+                add_wishlist={add_wishlist}
+                add_item={add_item}
+                setquery={setquery}
+              />
+            }
+          >
+            <Route path="drop_down/:category" element={<Drop_down />} />
 
-        {setquery}/>} >
-          <Route path='drop_down/:category'  element={<Drop_down />}  />
+            <Route
+              path="/"
+              element={
+                <Home
+                  loading={loading}
+                  add_wishlist_cart={add_wishlist_cart}
+                  add_cart={add_cart}
+                  add_wishlist={add_wishlist}
+                  add_item={add_item}
+                  product={filter_ittems}
+                  setadd_item={setadd_item}
+                  setadd_wishlist={setadd_wishlist}
+                />
+              }
+            />
 
-          <Route path='/'  element={<Home loading={loading}  add_wishlist_cart={add_wishlist_cart} 
-          add_cart={add_cart} add_wishlist={add_wishlist} add_item={add_item} 
-          product={filter_ittems} setadd_item={setadd_item}
-          setadd_wishlist={setadd_wishlist}
-          
-          />}  />
+            <Route path="product/:id" element={<Products />} />
 
-          <Route path='product/:id'  element={<Products />}  />
+            <Route
+              path="/toggle"
+              element={<Toggle_item add_item={add_item} />}
+            />
 
-          <Route path='/toggle'  element={<Toggle_item add_item={add_item}/>}  />
-
-          <Route path='/wishlist'  element={<Wishlist add_wishlist={add_wishlist}/>}  />
-          {/* <Route path='/'  element={<Home/>}  /> */}
-      </Route>
-    </Routes>
-    </BrowserRouter>
-
+            <Route
+              path="/wishlist"
+              element={<Wishlist add_wishlist={add_wishlist} />}
+            />
+            {/* <Route path='/'  element={<Home/>}  /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,44 +1,41 @@
-
-
-import { useState } from 'react';
-import Cart from './Product_list';
+import { useState } from "react";
+import Cart from "./Product_list";
 // import { Spin } from 'antd';
-import { Alert, Flex, Spin } from 'antd';
+import { Alert, Flex, Spin } from "antd";
 // import { Audio } from 'react-loader-spinner'
 // const sort_category = ["electronics","jewelery","men's clothing","women's clothing"]
 // console.log('sort_category',sort_category)
 
-function Home({add_cart,  add_item, add_wishlist_cart, product ,loading}) {
-    const contentStyle = {
-        padding: 50,
-        background: 'rgba(0, 0, 0, 0.05)',
-        borderRadius: 4,
-      };
-    const content = <div style={contentStyle} />;
+function Home({ add_cart, add_item, add_wishlist_cart, product, loading }) {
+  const contentStyle = {
+    padding: 50,
+    background: "rgba(0, 0, 0, 0.05)",
+    borderRadius: 4,
+  };
+  const content = <div style={contentStyle} />;
 
-    const [sort_items, setort_items] = useState('')
-    const [selectedCategory, setselectedCategory] = useState(null)
-   
-    
-        const remove_cart = (id) => {
-          const remove_from_cart =   add_item.filter((item) => item.id !== id)
-          setadd_item([...add_item, remove_from_cart])
-    }
-    const categories = Array.from(
-        new Set(product.map((res) => res.category))
-    )
-    console.log('categories.........',categories)
+  const [sort_items, setort_items] = useState("");
+  const [selectedCategory, setselectedCategory] = useState(null);
 
-    const categoryOption = categories.map((category) => ({
-        value : category,
-        label : category
-    }))
+  const remove_cart = (id) => {
+    const remove_from_cart = add_item.filter((item) => item.id !== id);
+    setadd_item([...add_item, remove_from_cart]);
+  };
+  const categories = Array.from(new Set(product.map((res) => res.category)));
+  console.log("categories.........", categories);
 
-    const filterProducts = selectedCategory ? product.filter((product) => product.category === selectedCategory.value) : product;
+  const categoryOption = categories.map((category) => ({
+    value: category,
+    label: category,
+  }));
 
-        return (
-        <div className='max-w-[1440px] m-auto'>
-         {/* <div className='mt-[151px] '>
+  const filterProducts = selectedCategory
+    ? product.filter((product) => product.category === selectedCategory.value)
+    : product;
+
+  return (
+    <div className="max-w-[1440px] m-auto">
+      {/* <div className='mt-[151px] '>
              <select
              option={categoryOption}
              placeholder='select a category'
@@ -46,20 +43,20 @@ function Home({add_cart,  add_item, add_wishlist_cart, product ,loading}) {
              value={selectedCategory}
              ></select>
         </div> */}
-      
-      {loading ?    <div className="flex justify-center ">
-        {/* <Spin  className='mt-[22%]' /> */}
-    <div className='mt-[22%]'>
-    <Flex gap="middle" vertical>
-    <Flex gap="middle">
-      <Spin tip="Loading" size="large">
-        {content}
-      </Spin>
-    </Flex>
 
-  </Flex>
-    </div>
-        {/* <Audio
+      {loading ? (
+        <div className="flex justify-center ">
+          {/* <Spin  className='mt-[22%]' /> */}
+          <div className="mt-[22%]">
+            <Flex gap="middle" vertical>
+              <Flex gap="middle">
+                <Spin tip="Loading" size="large">
+                  {content}
+                </Spin>
+              </Flex>
+            </Flex>
+          </div>
+          {/* <Audio
   height="80"
   width="80"
   radius="9"
@@ -68,34 +65,49 @@ function Home({add_cart,  add_item, add_wishlist_cart, product ,loading}) {
   wrapperStyle
   wrapperClass
 /> */}
-   </div>:
-         <div className=' '> 
-         <div className='mt-[120px] mb-[32px]  flex justify-end mr-[135px]'>
-    <select
-        onChange={(e) => setselectedCategory(categoryOption.find(option => option.value === e.target.value))}
-        value={selectedCategory?.value || ''}
-    >
-        <option value="" disabled>
-            Select a category
-        </option>
-        {categoryOption.map((option) => (
-            <option key={option.value} value={option.value}>
-                {option.label}
-            </option>
-        ))}
-    </select>
-</div >
-  <div className='flex flex-wrap  justify-center gap-12  mt-[15px]s' >
-  {
-    filterProducts <= 0 ? <h1>Product is not found</h1> :
-        filterProducts.map((item) => (
-        <Cart key={item.id} item = {item } add_cart={add_cart} add_wishlist_cart={add_wishlist_cart} add_item={add_item} remove_cart={remove_cart}/>
-    )) 
-}
-  </div>
-
-</div>}
         </div>
-          )
+      ) : (
+        <div className=" ">
+          <div className="mt-[120px] mb-[32px]  flex justify-end mr-[135px]">
+            <select
+              onChange={(e) =>
+                setselectedCategory(
+                  categoryOption.find(
+                    (option) => option.value === e.target.value
+                  )
+                )
+              }
+              value={selectedCategory?.value || ""}
+            >
+              <option value="" disabled>
+                Select a category
+              </option>
+              {categoryOption.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex flex-wrap  justify-center gap-12  mt-[15px]s">
+            {filterProducts <= 0 ? (
+              <h1>Product is not found</h1>
+            ) : (
+              filterProducts.map((item) => (
+                <Cart
+                  key={item.id}
+                  item={item}
+                  add_cart={add_cart}
+                  add_wishlist_cart={add_wishlist_cart}
+                  add_item={add_item}
+                  remove_cart={remove_cart}
+                />
+              ))
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
-export default Home
+export default Home;
